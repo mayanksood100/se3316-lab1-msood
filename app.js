f7ffbae914d7c7f30f064f1d7eea7dbb971d40ee
Mayank Sood <mayanksood100@gmail.com>
2020-09-12 14:31:38 -0400

    Validating Funtionality of Name Field
    I added an event listener to the second search button to validate if the name that the user enter is shorter than 20 characters and only includes "A-Z or a-z" via a regular expression.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let number = document.getElementById("inputnum");
+let name = document.getElementById("name");
+let letters = /^[a-zA-Z\s]+$/;
 let button1 = document.getElementById("bttn1");
+let button2 = document.getElementById("bttn2");
 
 
 button1.addEventListener('click', function(){
@@ -7,4 +10,18 @@ button1.addEventListener('click', function(){
         alert("Input Not Valid. Please enter a number between 1 and 20.");
        }
        number.value = "";
+});
+
+button2.addEventListener('click', function(){
+    if(name.value.length <= 20 && name.value.match(letters)){
+        alert("You typed " +  name.value);
+    }
+    else if(name.value == ""){
+        alert("You typed nothing.");
+    }
+    else{
+        alert("Input is not valid. Please enter character A-Z or a-z only and shorter than 20 characters.");
+        
+    }
+    name.value = "";
 });

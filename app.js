929560807a0a84fecd72c3cc369ca2da76b63d28
Mayank Sood <mayanksood100@gmail.com>
2020-09-16 00:59:49 -0400

    Added Comments to Javascript File
    I added some questions to the Javascript file to allow others to easily understand how I implemented my search functions.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Declaring different variables and using the Document Object Model to retrieve essential elements of the HTML that are needed such as the two search boxes, the two buttons the lis, etc.
 let number = document.getElementById("inputnum");
 let name = document.getElementById("name");
 let letters = /^[a-zA-Z\s]+$/;
@@ -8,14 +9,15 @@ let button1 = document.getElementById("bttn1");
 let button2 = document.getElementById("bttn2");
 let tracker = 0;
 
-console.log("Tracker : " + tracker);
-
+//Adding an Event Listener to the Number Search Button to validate the required criteria.
 button1.addEventListener('click', function(event){
     if (isNaN(number.value) || number.value < 1 || number.value > 20) {
         number.value = "";
         alert("Input Not Valid. Please enter a number between 1 and 20.");
         Event.stop(event);
        }
+       //If the number does follow the given criteria (being between 1 and 20), the for loop will try to find a match with the span tag, it will alert the users of all of the matches (with a max of 5) and show only those matches since all of the other li elements will have their style display set to none.
+
        for(let i=0; i<spanNums.length; i++){
        
         if(spanNums[i].textContent.indexOf(parseInt(number.value))!=-1 && tracker<5){
@@ -33,6 +35,7 @@ button1.addEventListener('click', function(event){
        number.value = "";
 });
 
+//Adding an Event Listener to the Name Search Button to validate the required criteria.
 button2.addEventListener('click', function(event){
     if(name.value.length <= 20 && name.value.match(letters)){
         console.log("You typed " +  name.value);
@@ -52,6 +55,7 @@ button2.addEventListener('click', function(event){
        Event.stop(event);
         
     }
+    //If the name does follow the given criteria (only letters A-Z or a-z and being shorter than 20 characters), the for loop will try to find a match with the h4 tag which hold the names of all the Pokemon and it will alert the users of all of the matches (with a max of 5 again) and show only those matches since all of the other li elements will have their style display set to none.
 
     for(let i=0; i<pokeNames.length; i++ ){
         if(pokeNames[i].textContent.toLowerCase().indexOf(name.value.toLowerCase())!=-1 && tracker<5){
@@ -66,6 +70,9 @@ button2.addEventListener('click', function(event){
 
     name.value = "";
 });
+
+
+//This keyup eventlistener will cause the respective button to be clicked thus performing the same function as the button. Since the keycode is 13 which corresponds to the "Enter" key, this will only happen when the enter key is pressed on any of the two searchboxes. 
 
 number.addEventListener("keyup", function(event) {
     if(event.keyCode === 13){
